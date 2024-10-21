66c236db807faef1f439a3aa38ffee1bb1ee9b80
Zohaifa <ultimatezrage@gmail.com>
2024-10-21 10:29:22 +0600

    Basic UI

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import kivy
 
 kivy.require('2.3.0')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.spinner import Spinner
+from kivy.uix.checkbox import CheckBox
+
+class Forms(App):
+    def build(self):
+        return FormLayout()
+#    def submit_form(self):
+ #       product_name = self.root.ids.product_name_input.text:
+
+class FormLayout(BoxLayout):
+    pass
+
+if __name__ == '__main__':
+   Forms().run()
