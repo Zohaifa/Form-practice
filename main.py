66538f98def6fdda45101b55a416fac2e22fab0d
Zohaifa <ultimatezrage@gmail.com>
2024-10-21 10:49:32 +0600

    Functionality added

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ from kivy.uix.checkbox import CheckBox
 class Forms(App):
     def build(self):
         return FormLayout()
-#    def submit_form(self):
- #       product_name = self.root.ids.product_name_input.text:
+    def submit_form(self):
+        product_name = self.root.ids.product_name_input.text
+        price = self.root.ids.price_input.text
+        category = self.root.ids.category_spinner.text
+        checkbox = self.root.ids.in_stock_checkbox.active
+
+        print(f"Product Name: {product_name}")
+        print(f"Price: {price}")
+        print(f"Category: {category}")
+        print(f"In Stock: {checkbox}")
 
 class FormLayout(BoxLayout):
     pass
