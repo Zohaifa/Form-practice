16f9c8d4780606e876888edc71d8b392de281be0
Zohaifa <ultimatezrage@gmail.com>
2024-10-25 10:36:45 +0600

    Saved data in a json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import kivy
 import re
+import json
 
 kivy.require('2.3.0')
 
@@ -14,7 +15,6 @@ from kivy.uix.button import Button
 class Forms(App):
     def build(self):
         return MyForm()
-
 
 class MyForm(BoxLayout):
     def __init__(self, **kwargs):
@@ -42,6 +42,7 @@ class MyForm(BoxLayout):
             return False, "This field cannot be empty"
         input_field.background_color = (1, 1, 1, 1)
         return True, ""
+
     def validate_numeric_input(self, input_field):
         try:
             value = float(input_field.text)
@@ -50,11 +51,13 @@ class MyForm(BoxLayout):
             return True, ""
         except ValueError:
             return False, "Please enter a valid number"
+
     def validate_email_address(self, input_field):
         pattern = r'^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         if not re.match(pattern, input_field.text):
             return False, "Please enter a valid email address"
         return True, ""
+
     def submit_form(self, instance):
         valid_name, name_error = self.validate_text_input(self.name_input)
         valid_price, price_error = self.validate_numeric_input(self.price_input)
@@ -73,6 +76,13 @@ class MyForm(BoxLayout):
             }
             self.display_data(form_data)
             self.save_data_to_file(form_data)
+            self.save_data_to_json(form_data)
+            self.clear_form()
+
+    def clear_form(self):
+        self.name_input.text = ""
+        self.price_input.text = ""
+        self.email_input.text = ""
 
     def display_data(self, form_data):
         self.error_label.text = f"Name: {form_data['name']}\nPrice: {form_data['price']}\nEmail:{form_data['email']}\n"
@@ -80,10 +90,20 @@ class MyForm(BoxLayout):
     def display_error(self, errors):
         error_messages = "\n".join([error for error in errors if error])
         self.error_label.text = error_messages
+
     def save_data_to_file(self, form_data):
         with open('form_data.txt', 'a') as file:
-            file.write(f"Name: {form_data['name']}, Price: {form_data['price']}, Email: {form_data['email']}")
+            file.write(f"Name: {form_data['name']}, Price: {form_data['price']}, Email: {form_data['email']}\n")
 
+    def save_data_to_json(self, form_data):
+        try:
+            with open("form_data.json", 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = []
+        data.append(form_data)
+        with open("form_data.json", 'w') as file:
+            json.dump(data, file, indent=4)
 
 if __name__ == '__main__':
    Forms().run()
